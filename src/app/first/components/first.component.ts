8788587e2562f69b651528cbbd2ac5fbf4d32af8
icosagon8 <dmpavlov8@yandex.ru>
2021-12-11 20:40:39 +0300

    feat: add FirstComponent

---
+++ src/app/first/components/first.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+import { Category, CoverType, Type } from '../models/first.models';
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.scss'],
+})
+export class FirstComponent {
+  name: string = 'The Lord of the Rings';
+  description: string =
+    'One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them.';
+  price: number = 20;
+  category: Category = Category.Fantasy;
+  isAvailable: boolean = true;
+  types: Type[] = [Type.Printed, Type.Audio];
+  coverTypes: CoverType[] = [CoverType.Hardcover, CoverType.Softcover];
+}
---
+++ src/app/first/models/first.models.ts
@@ -0,0 +1,16 @@
+export enum Category {
+  Fantasy = 'Fantasy',
+  Detective = 'Detective',
+  Romance = 'Romance',
+}
+
+export enum Type {
+  Audio = 'Audio',
+  Electronic = 'Electronic',
+  Printed = 'Printed',
+}
+
+export enum CoverType {
+  Softcover = 'Softcover',
+  Hardcover = 'Hardcover',
+}
