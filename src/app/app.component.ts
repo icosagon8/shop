256e44742f7ec78c7a2fb4435e1bc725c7d8ccf4
icosagon8 <dmpavlov8@yandex.ru>
2022-01-09 13:54:23 +0300

    feat: add h1 heading element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Shop';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('appTitle')
+  appTitle!: ElementRef<HTMLHeadingElement>;
+
+  ngAfterViewInit() {
+    this.appTitle.nativeElement.textContent = 'Shop';
+  }
 }
