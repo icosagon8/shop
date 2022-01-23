dc3b5d13e02852421ce1c7bc0e3be785c859efe5
icosagon8 <dmpavlov8@yandex.ru>
2022-01-23 16:49:03 +0300

    fix: remove CommonModule from MaterialModule

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
-  exports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatCheckboxModule, MatSelectModule],
+  exports: [MatCardModule, MatButtonModule, MatIconModule, MatCheckboxModule, MatSelectModule],
 })
 export class MaterialModule {}
